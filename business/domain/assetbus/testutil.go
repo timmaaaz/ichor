2106c993f6b9fa786de29c0fced5ee1e444f5014
Jacob Timmer <jake@superiortech.io>
2024-10-09 14:16:17 -0400

    [chore] Biz layer for assets complete
    Title says all.

---
+++ business/domain/assetbus/testutil.go
@@ -0,0 +1,75 @@
+package assetbus
+
+import (
+	// "context"
+	"context"
+	"fmt"
+	"math/rand"
+	"sort"
+
+	"github.com/google/uuid"
+	"github.com/timmaaaz/ichor/business/domain/assetbus/types"
+)
+
+// TestNewAssets is a helper method for testing.
+func TestNewAssets(n int, typeIDs []uuid.UUID, conditionIDs []uuid.UUID, userID uuid.UUID) []NewAsset {
+	newAssets := make([]NewAsset, n)
+
+	idx := rand.Intn(10000)
+	for i := 0; i < n; i++ {
+		idx++
+
+		estPrice, err := types.ParseMoney(fmt.Sprintf("%.2f", rand.Float64()*1000))
+		if err != nil {
+			panic(err)
+		}
+
+		price, err := types.ParseMoney(fmt.Sprintf("%.2f", rand.Float64()*1000))
+		if err != nil {
+			panic(err)
+		}
+
+		maintenanceInterval, _ := types.ParseInterval("14 days")
+		lifeExpectancy, _ := types.ParseInterval("1 year")
+
+		na := NewAsset{
+			TypeID:              typeIDs[rand.Intn(len(typeIDs))],
+			ConditionID:         conditionIDs[rand.Intn(len(conditionIDs))],
+			Name:                fmt.Sprintf("Name%d", idx),
+			EstPrice:            estPrice,
+			Price:               price,
+			MaintenanceInterval: maintenanceInterval,
+			LifeExpectancy:      lifeExpectancy,
+			SerialNumber:        fmt.Sprintf("SerialNumber%d", idx),
+			ModelNumber:         fmt.Sprintf("ModelNumber%d", idx),
+			IsEnabled:           true,
+			CreatedBy:           userID,
+		}
+
+		newAssets[i] = na
+	}
+
+	return newAssets
+}
+
+// TestSeedAssets is a helper method for testing.
+func TestSeedAssets(ctx context.Context, n int, typeIDs []uuid.UUID, conditionIDs []uuid.UUID, userID uuid.UUID, api *Business) ([]Asset, error) {
+	newAssets := TestNewAssets(n, typeIDs, conditionIDs, userID)
+
+	assets := make([]Asset, len(newAssets))
+	for i, na := range newAssets {
+		asset, err := api.Create(ctx, na)
+		if err != nil {
+			return nil, fmt.Errorf("seeding asset: idx: %d : %w", i, err)
+		}
+
+		assets[i] = asset
+	}
+
+	// Sort by name
+	sort.Slice(assets, func(i, j int) bool {
+		return assets[i].Name <= assets[j].Name
+	})
+
+	return assets, nil
+}
--- business/sdk/dbtest/dbtest.go
+++ business/sdk/dbtest/dbtest.go
@@ -11,6 +11,8 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/timmaaaz/ichor/business/domain/approvalstatusbus"
 	"github.com/timmaaaz/ichor/business/domain/approvalstatusbus/stores/approvalstatusdb"
+	"github.com/timmaaaz/ichor/business/domain/assetbus"
+	"github.com/timmaaaz/ichor/business/domain/assetbus/stores/assetdb"
 	"github.com/timmaaaz/ichor/business/domain/assetconditionbus"
 	"github.com/timmaaaz/ichor/business/domain/assetconditionbus/stores/assetconditiondb"
 	"github.com/timmaaaz/ichor/business/domain/assettypebus"
@@ -48,6 +50,7 @@ type BusDomain struct {
 	Home           *homebus.Business
 	AssetType      *assettypebus.Business
 	AssetCondition *assetconditionbus.Business
+	Asset          *assetbus.Business
 	Product        *productbus.Business
 	User           *userbus.Business
 	Country        *countrybus.Business
@@ -66,6 +69,7 @@ func newBusDomains(log *logger.Logger, db *sqlx.DB) BusDomain {
 
 	assetTypeBus := assettypebus.NewBusiness(log, delegate, assettypedb.NewStore(log, db))
 	assetConditionBus := assetconditionbus.NewBusiness(log, delegate, assetconditiondb.NewStore(log, db))
+	assetBus := assetbus.NewBusiness(log, delegate, assetdb.NewStore(log, db))
 
 	userBus := userbus.NewBusiness(log, delegate, usercache.NewStore(log, userdb.NewStore(log, db), time.Hour))
 	productBus := productbus.NewBusiness(log, userBus, delegate, productdb.NewStore(log, db))
@@ -80,6 +84,7 @@ func newBusDomains(log *logger.Logger, db *sqlx.DB) BusDomain {
 		Home:           homeBus,
 		AssetType:      assetTypeBus,
 		AssetCondition: assetConditionBus,
+		Asset:          assetBus,
 		Product:        productBus,
 		User:           userBus,
 		Country:        countryBus,
