ae87c430e8d0ab70618c7a2e0bf49507f8eca033
Jake Timmer <jaketimmer@Jakes-MacBook-Pro.local>
2025-02-18 16:09:01 -0500

    [chore] Working on perms
    Title says all.

---
+++ business/domain/permissions/rolebus/stores/roledb/model.go
@@ -0,0 +1,36 @@
+package roledb
+
+import (
+	"github.com/google/uuid"
+	"github.com/timmaaaz/ichor/business/domain/permissions/rolebus"
+)
+
+type role struct {
+	ID          uuid.UUID `db:"role_id"`
+	Name        string    `db:"name"`
+	Description string    `db:"description"`
+}
+
+func toDBRole(bus rolebus.Role) role {
+	return role{
+		ID:          bus.ID,
+		Name:        bus.Name,
+		Description: bus.Description,
+	}
+}
+
+func toBusRole(db role) rolebus.Role {
+	return rolebus.Role{
+		ID:          db.ID,
+		Name:        db.Name,
+		Description: db.Description,
+	}
+}
+
+func toBusRoles(dbs []role) []rolebus.Role {
+	roles := make([]rolebus.Role, len(dbs))
+	for i, db := range dbs {
+		roles[i] = toBusRole(db)
+	}
+	return roles
+}
