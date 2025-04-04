9af79b1526c9949a6736225504de29c91e4f03c0
Case Brophy <cbrophy@superiortech.io>
2025-04-04 09:44:21 -0600

    [feat] Inventory Transactions
    - CRUD Operations for inventory transaction

---
+++ api/domain/http/movement/inventorytransactionapi/filter.go
@@ -0,0 +1,29 @@
+package inventorytransactionapi
+
+import (
+	"net/http"
+
+	"github.com/timmaaaz/ichor/app/domain/movement/inventorytransactionapp"
+)
+
+func parseQueryParams(r *http.Request) (inventorytransactionapp.QueryParams, error) {
+	values := r.URL.Query()
+
+	filter := inventorytransactionapp.QueryParams{
+		Page:                   values.Get("page"),
+		Rows:                   values.Get("rows"),
+		OrderBy:                values.Get("orderBy"),
+		InventoryTransactionID: values.Get("transaction_id"),
+		ProductID:              values.Get("product_id"),
+		LocationID:             values.Get("location_id"),
+		UserID:                 values.Get("user_id"),
+		Quantity:               values.Get("quantity"),
+		TransactionType:        values.Get("transaction_type"),
+		ReferenceNumber:        values.Get("reference_number"),
+		TransactionDate:        values.Get("transaction_date"),
+		CreatedDate:            values.Get("created_date"),
+		UpdatedDate:            values.Get("updated_date"),
+	}
+
+	return filter, nil
+}
